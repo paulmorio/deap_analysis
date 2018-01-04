f02550c79a16e26b4042aa2c112d44330c5e98c1
paulmorio <schererpaul@rocketmail.com>
2018-01-04 18:52:54 +0000

    Overview stats

--- data_load_construction.py
+++ data_load_construction.py
@@ -1,4 +1,8 @@
 # DEAP preprocessed data construction
 
+# Lets get a brief overview of one piece of data
 import cPickle
-x = cPickle.load(open('/deap_data/data_preprocessed_python/s01.dat', 'rb'))
+x = cPickle.load(open('deap_data/data_preprocessed_python/s01.dat', 'rb'))
+print type(x)
+print x['labels'].shape
+print x['data'].shape
