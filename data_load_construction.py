27b63f271326680b0b6f9dac63ba1ff8dc51f986
paulmorio <schererpaul@rocketmail.com>
2018-01-04 21:49:35 +0000

    Wrote code to create a more packaged full dataset

--- data_load_construction.py
+++ data_load_construction.py
@@ -7,3 +7,18 @@ print type(x)
 print x['labels'].shape
 print x['data'].shape
 
+data = []
+for i in range(1,33):
+	if i < 10:
+		expId = "0" + str(i)
+	else:
+		expId = str(i)
+
+	file_to_load = "deap_data/data_preprocessed_python/s" + expId + ".dat"
+	par_experiment_data = cPickle.load(open(file_to_load, 'rb'))
+	print(expId)
+	data.append(par_experiment_data)
+
+f = open('deap_data/data_preprocessed_python/all_32.dat', 'wb')
+cPickle.dump(data, f, protocol=cPickle.HIGHEST_PROTOCOL)
+f.close()
