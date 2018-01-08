03c10f712b90e9811666008045e7bceed2d8edcc
paulmorio <schererpaul@rocketmail.com>
2018-01-08 18:11:20 +0000

    preprocessing and grabbing of features for channels

--- signalpreprocess.py
+++ signalpreprocess.py
@@ -1,2 +1,61 @@
 # This module is dedicated to summarizing single channel signal data and extracting features from it.
 
+import numpy as np
+
+def normalise_signal(signal):
+	"""Normalise the signal to zero mean, unit variance"""
+	signal_mean = np.mean(signal)
+	signal_stdev = np.std(signal)
+	signal_normed = [((x-signal_mean)/signal_stdev) for x in signal]
+	return(signal_normed)
+
+def mean(signal):
+	""" The mean of the raw signal"""
+	return(np.mean(signal))
+
+def std_dev(signal):
+	"""The standard deviation of the raw signal"""
+	return(np.std(signal))
+
+def first_differences(signal):
+	"""The mean of the absolute values of the first differences of the raw signal"""
+	# build the first differences list
+	first_diff = []
+	for i in range(0,len(signal)-1):
+		first_diff.append(abs(signal[i+1]-signal[i]))
+
+	fd_sum = sum(first_diff)
+	delta = float(fd_sum)/(len(signal)-1)
+	return(delta)
+
+def norm_first_differences(signal):
+	"""The mean of the absolute values of the first differences of the normalized signal"""
+	signal_normed = normalise_signal(signal)
+	first_diff = []
+	for i in range(0,len(signal_normed)-1):
+		first_diff.append(abs(signal_normed[i+1]-signal_normed[i]))
+
+	fd_sum = sum(first_diff)
+	delta = float(fd_sum)/(len(signal_normed)-1)
+	return(delta)
+
+def second_differences(signal):
+	"""The mean of the absolute values of the second differences of the raw signal"""
+	sec_diff = []
+	for i in range(0,len(signal)-2):
+		sec_diff.append(abs(signal[i+2]-signal[i]))
+
+	fd_sum = sum(sec_diff)
+	delta = float(fd_sum)/(len(signal)-2)
+	return(delta)
+
+def norm_second_differences(signal):
+	"""The mean of the absolute values of the second differences of the normalized signal"""
+	signal_normed = normalise_signal(signal)
+	sec_diff = []
+	for i in range(0,len(signal_normed)-2):
+		sec_diff.append(abs(signal_normed[i+2]-signal_normed[i]))
+
+	fd_sum = sum(sec_diff)
+	delta = float(fd_sum)/(len(signal_normed)-2)
+	return(delta)
