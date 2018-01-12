8ad4e1524eb3b86db33dded2d992e1cbdce92ebf
paulmorio <schererpaul@rocketmail.com>
2018-01-12 00:06:46 +0000

    Lr assymetries in correct bands

--- eeg_fe_construction.py
+++ eeg_fe_construction.py
@@ -38,37 +38,29 @@ for person in participants:
 		# Our data vector
 		x = []
 
-		# # The left right assymetry signals alpha
-		# lr_pfl = ef.lr_assymetry_pfl(channels_data)
-		# lr_ears = ef.lr_assymetry_ears(channels_data)
-		# lr_back = ef.lr_assymetry_back(channels_data)
-		# x = x + lr_pfl.tolist() + lr_ears.tolist() + lr_back.tolist()
+		# The left right assymetry signals alpha
+		lr_pfl = ef.lr_assymetry_pfl(channels_data, "alpha")
+		lr_ears = ef.lr_assymetry_ears(channels_data, "alpha")
+		lr_back = ef.lr_assymetry_back(channels_data, "alpha")
+		x = x + lr_pfl.tolist() + lr_ears.tolist() + lr_back.tolist()
 
-		# # The left right assymetry signals theta
-		# lr_pfl = ef.lr_assymetry_pfl_t(channels_data)
-		# lr_ears = ef.lr_assymetry_ears_t(channels_data)
-		# lr_back = ef.lr_assymetry_back_t(channels_data)
-		# x = x + lr_pfl.tolist() + lr_ears.tolist() + lr_back.tolist()
+		# The left right assymetry signals theta
+		lr_pfl = ef.lr_assymetry_pfl(channels_data, "theta")
+		lr_ears = ef.lr_assymetry_ears(channels_data, "theta")
+		lr_back = ef.lr_assymetry_back(channels_data, "theta")
+		x = x + lr_pfl.tolist() + lr_ears.tolist() + lr_back.tolist()
+
+		# The left right assymetry signals theta
+		lr_pfl = ef.lr_assymetry_pfl(channels_data, "beta")
+		lr_ears = ef.lr_assymetry_ears(channels_data, "beta")
+		lr_back = ef.lr_assymetry_back(channels_data, "beta")
+		x = x + lr_pfl.tolist() + lr_ears.tolist() + lr_back.tolist()
 
 		# energy log sum ratios eeg_w for all the channels
 		eeg_w_list = []
 		for signal in channels_data:
 			eeg_w_list.append(ef.eeg_w_beta(signal))
 		x = x + eeg_w_list
-
-		# # powerspectra for all the channels
-		# powerspectra = []
-		# for signal in channels_data:
-		# 	powerspectra.extend(ef.power_spectrums(signal))
-		# x = x + powerspectra
-
-		# # simple stats
-		# sstats = []
-		# for signal in channels_data:
-		# 	m,s,nfd,nsd, mini, maxi = sp.package_deal_signal_mm(signal)
-		# 	sstats += [m,s,nfd,nsd, mini, maxi]
-		# x = x + sstats
-
 
 		X.append(x)
 	print person
